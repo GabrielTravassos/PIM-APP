9275d08638280b79206713f1e2629c2ca435bc9c
Gabriel Soares Travassos <42143734+GabrielTravassos@users.noreply.github.com>
2021-11-25 21:53:05 -0300

    Integrando banco de dados Firestone Componentes

--- app/src/main/java/com/example/pim/FormLogin.java
+++ app/src/main/java/com/example/pim/FormLogin.java
@@ -4,12 +4,18 @@ import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class FormLogin extends AppCompatActivity {
 
     private TextView text_tela_cadastro;
-    
+    private EditText edit_email,edit_senha;
+    private Button bt_entrar;
+    private ProgressBar progressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,5 +36,9 @@ public class FormLogin extends AppCompatActivity {
 
     private void IniciarComponentes(){
         text_tela_cadastro = findViewById(R.id.text_tela_cadastro);
+        edit_email = findViewById(R.id.edit_email);
+        edit_senha = findViewById(R.id.edit_senha);
+        bt_entrar = findViewById(R.id.bt_entrar);
+        progressBar = findViewById(R.id.progressbar);
     }
 }
